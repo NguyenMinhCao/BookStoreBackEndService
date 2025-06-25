5c15d413d31c68a4c33169f2dd09f72c122f3cdf
NguyenMinhCao <caonguyenlaptrinh@gmail.com>
2025-06-25 12:05:05 +0700

    refactor: enhance security configuration and authentication flow

--- BookStoreBackEnd/src/main/java/vn/phancao/bookstorebackend/controller/AuthController.java
+++ BookStoreBackEnd/src/main/java/vn/phancao/bookstorebackend/controller/AuthController.java
@@ -2,9 +2,6 @@ package vn.phancao.bookstorebackend.controller;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 import vn.phancao.bookstorebackend.dto.request.SignInRequest;
 import vn.phancao.bookstorebackend.dto.response.ResponseData;
@@ -15,12 +12,9 @@ import vn.phancao.bookstorebackend.dto.response.SignInResponse;
 @RequiredArgsConstructor
 public class AuthController {
 
-    public final AuthenticationManager authenticationManager;
 
     @PostMapping("/sign-in")
     ResponseData<SignInResponse> signIn(@Valid @RequestBody SignInRequest request) {
-        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword()));
-        System.out.println(authentication.getPrincipal());
         return null;
     }
 }
